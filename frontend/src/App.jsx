997777d06eef686fa34313b62e2b62e3b8cdc2b7
Andrei <chindrisandrei2005@gmail.com>
2025-10-24 14:49:00 +0300

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import LetterGlitch from './components/LetterGlitch'
+import LiquidNav from './components/LiquidNav'
+
+const App = () => {
+  return (
+    <div className='flex-1'>
+      <div className="absolute inset-0 -z-10">
+        <LetterGlitch
+          glitchSpeed={50}
+          centerVignette={false}
+          outerVignette={false}
+          smooth={true}
+        />
+      </div>
+      <main className='relative z-10 w-full items-center justify-center flex flex-col gap-10 pt-10'>
+        <LiquidNav />
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/LiquidNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LiquidNav = () => {
+  return (
+    <div className='w-[70%] h-12 bg-white/10 backdrop-blur-md rounded-full border border-white/10 flex items-center justify-start px-10'>
+        <h1 className='text-white text-xl font-bold'>Campaign Play Processor</h1>
+    </div>
+  )
+}
+
+export default LiquidNav
